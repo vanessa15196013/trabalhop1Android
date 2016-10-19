a6a4886ee0bf5c9c603655edce25ebd665a61ef2
vanessa.alves <vanessadossantosalves52@gmail.com>
2016-10-18 23:40:07 -0200

    P1 Android

---
+++ app/src/main/java/com/example/vanessaalves/provaalves/CadastroAlunoActivity.java
@@ -0,0 +1,13 @@
+package com.example.vanessaalves.provaalves;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_aluno);
+    }
+}
---
+++ app/src/main/java/com/example/vanessaalves/provaalves/ProgressoAulaActivity.java
@@ -0,0 +1,13 @@
+package com.example.vanessaalves.provaalves;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProgressoAulaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progresso_aula);
+    }
+}
